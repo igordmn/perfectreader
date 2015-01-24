d76f515ca43c8e19c478d1a6eb2c14adb33849de
Igor Demin <igordmn@gmail.com>
2015-01-24 19:14:03 +0300

    PR-174 Реализовать настройки управления

--- app/src/main/java/com/dmi/perfectreader/control/SimpleCommand.java
+++
@@ -1,7 +0,0 @@
-package com.dmi.perfectreader.control;
-
-public enum SimpleCommand {
-    TOGGLE_MENU,
-    GO_NEXT_PAGE,
-    GO_PREVIEW_PAGE
-}
--- app/src/main/java/com/dmi/perfectreader/control/ValueCommand.java
+++
@@ -1,5 +0,0 @@
-package com.dmi.perfectreader.control;
-
-public enum ValueCommand {
-    CHANGE_FONT_SIZE
-}
