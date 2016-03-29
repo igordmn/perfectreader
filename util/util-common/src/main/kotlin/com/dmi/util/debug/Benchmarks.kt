7e0e225ab200c35c9d925ac196f136b48becbbe1
Igor Demin <igordmn@gmail.com>
2016-03-29 03:14:03 +0300

    Рефакторинг

--- util/util-common/src/main/kotlin/com/dmi/util/debug/Benchmarks.kt
+++ util/util-common/src/main/kotlin/com/dmi/util/debug/Benchmarks.kt
@@ -1,12 +1,13 @@
 package com.dmi.util.debug
 
-import java.lang.System.currentTimeMillis
+import java.lang.System.nanoTime
+
 
 inline fun measureTime(name: String = "", run: () -> Unit) {
-    val t1 = currentTimeMillis()
+    val t1 = nanoTime()
     run()
-    val t2 = currentTimeMillis()
-    val time = t2 - t1
+    val t2 = nanoTime()
+    val time = (t2 - t1) / 1E6
 
     if (name != "") {
         println("$name time: $time")
