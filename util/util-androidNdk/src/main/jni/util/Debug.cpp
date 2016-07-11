a88ea1f8fb9da49df775dc6ebe1fb6005a5b91a5
Igor Demin <igordmn@gmail.com>
2016-07-11 22:37:07 +0300

    Рефакторинг

--- util/util-androidNdk/src/main/jni/util/Debug.cpp
+++ util/util-androidNdk/src/main/jni/util/Debug.cpp
@@ -5,7 +5,7 @@
 #include "JniUtils.h"
 
 namespace dmi {
-    void abortWithLog(const char *file, int line, const char *message) {
+    void abortWithLog(const char *file, int line, const char *message, int errorCode) {
         const char *fileName =
                 strrchr(file, '/') ? strrchr(file, '/') + 1 :
                 strrchr(file, '\\') ? strrchr(file, '\\') + 1 :
@@ -15,10 +15,15 @@ namespace dmi {
         JNIEnv *env = scope.env();
 
         std::string javaStackTrace = dmi::jniUtils::getJavaStackTrace(env);
+
         if (message != 0) {
             __android_log_print(
                     ANDROID_LOG_ERROR, "utilAndroid", "Check failed: %s. %s:%d\n%s",
                     message, fileName, line, javaStackTrace.c_str());
+        } else if (errorCode != 0) {
+            __android_log_print(
+                    ANDROID_LOG_ERROR, "utilAndroid", "Check failed. Error code: %d. %s:%d\n%s",
+                    errorCode, fileName, line, javaStackTrace.c_str());
         } else {
             __android_log_print(
                     ANDROID_LOG_ERROR, "utilAndroid", "Check failed. %s:%d\n%s",
--- util/util-androidNdk/src/main/jni/util/Debug.h
+++ util/util-androidNdk/src/main/jni/util/Debug.h
@@ -1,21 +1,25 @@
 #pragma once
 
-#define LOG(level, text, ...) __android_log_print(level, "typoweb", text, ## __VA_ARGS__);
-#define LOGD(text, ...) __android_log_print(ANDROID_LOG_DEBUG, "typoweb", text, ## __VA_ARGS__);
-#define LOGI(text, ...) __android_log_print(ANDROID_LOG_INFO, "typoweb", text, ## __VA_ARGS__);
-#define LOGW(text, ...) __android_log_print(ANDROID_LOG_WARN, "typoweb", text, ## __VA_ARGS__);
-#define LOGE(text, ...) __android_log_print(ANDROID_LOG_ERROR, "typoweb", text, ## __VA_ARGS__);
+#include <android/log.h>
+
+#define LOG(level, text, ...) __android_log_print(level, "utilAndroid", text, ## __VA_ARGS__);
+#define LOGD(text, ...) __android_log_print(ANDROID_LOG_DEBUG, "utilAndroid", text, ## __VA_ARGS__);
+#define LOGI(text, ...) __android_log_print(ANDROID_LOG_INFO, "utilAndroid", text, ## __VA_ARGS__);
+#define LOGW(text, ...) __android_log_print(ANDROID_LOG_WARN, "utilAndroid", text, ## __VA_ARGS__);
+#define LOGE(text, ...) __android_log_print(ANDROID_LOG_ERROR, "utilAndroid", text, ## __VA_ARGS__);
 
 namespace dmi {
-    void abortWithLog(const char *file, int line, const char *message = 0);
+    void abortWithLog(const char *file, int line, const char *message = 0, int errorCode = 0);
 }
 
 #define CHECK(condition)  \
     if (!(condition)) {   \
-        abortWithLog(__FILE__, __LINE__); \
+        dmi::abortWithLog(__FILE__, __LINE__); \
     }
 
-#define CHECKM(condition, message)  \
-    if (!(condition)) {   \
-        abortWithLog(__FILE__, __LINE__, message); \
-    }
+#define CHECKE(callAndGetErrCode)  { \
+    int errCode = callAndGetErrCode; \
+    if (errCode != 0) {   \
+        dmi::abortWithLog(__FILE__, __LINE__, 0, errCode); \
+    } \
+};
