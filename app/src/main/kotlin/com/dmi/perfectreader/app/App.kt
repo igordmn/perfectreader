a6c08821fa8dce4416338c737dc826ca552a0ac5
Igor Demin <igordmn@gmail.com>
2016-07-12 00:30:55 +0300

    Удален NDK модуль

--- app/src/main/kotlin/com/dmi/perfectreader/app/App.kt
+++ app/src/main/kotlin/com/dmi/perfectreader/app/App.kt
@@ -9,7 +9,6 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        System.loadLibrary("utilAndroid")
         initAndroidPlatform(this)
         objects = AppObjects(this)
     }
--- util/util-androidNdk/src/main/jni/EntryPoint.cpp
+++
@@ -1,8 +0,0 @@
-#include "util/JniUtils.h"
-
-using namespace dmi;
-
-jint JNI_OnLoad(JavaVM *vm, void *) {
-    dmi::registerJniUtils(vm);
-    return JNI_VERSION_1_6;
-}
