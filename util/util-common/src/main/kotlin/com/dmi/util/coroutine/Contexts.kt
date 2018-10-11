1f309d175ed4fe7075ce8811cfaddc2ba30bb38c
Igor Demin <igordmn@gmail.com>
2018-10-11 18:18:42 +0300

    Refactoring

--- util/util-common/src/main/kotlin/com/dmi/util/coroutine/Contexts.kt
+++ util/util-common/src/main/kotlin/com/dmi/util/coroutine/Contexts.kt
@@ -1,6 +1,9 @@
 package com.dmi.util.coroutine
 
 import com.dmi.util.lang.threadLocal
+import kotlin.coroutines.AbstractCoroutineContextElement
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
 
 var threadContext: CoroutineContext by threadLocal()
@@ -12,4 +15,19 @@ fun initThreadContext(threadContext: CoroutineContext) {
     require(!threadContextInit)
     com.dmi.util.coroutine.threadContext = threadContext
     threadContextInit = true
+}
+
+fun CoroutineContext.wrap(wrapBlock: (() -> Unit) -> Unit): CoroutineContext {
+    class WrapContinuation<T>(val cont: Continuation<T>) : Continuation<T> by cont {
+        override fun resumeWith(result: Result<T>) = wrapBlock {
+            cont.resumeWith(result)
+        }
+    }
+
+    return this + object : AbstractCoroutineContextElement(ContinuationInterceptor), ContinuationInterceptor {
+        override fun <T> interceptContinuation(continuation: Continuation<T>): Continuation<T> {
+            val wrapped = WrapContinuation(continuation)
+            return this@wrap[ContinuationInterceptor]?.interceptContinuation(wrapped) ?: wrapped
+        }
+    }
 }
--- util/util-common/src/main/kotlin/com/dmi/util/scope/Observable.kt
+++ util/util-common/src/main/kotlin/com/dmi/util/scope/Observable.kt
@@ -1,5 +1,6 @@
 package com.dmi.util.scope
 
+import com.dmi.util.coroutine.wrap
 import com.dmi.util.lang.ReadWriteProperty2
 import com.dmi.util.lang.threadLocal
 import com.dmi.util.lang.value
@@ -7,9 +8,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlin.coroutines.AbstractCoroutineContextElement
-import kotlin.coroutines.Continuation
-import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
 import kotlin.reflect.KMutableProperty0
 
@@ -42,20 +40,7 @@ private class CallContext {
             afterBlock()
         }
 
-        fun wrapContext(context: CoroutineContext): CoroutineContext {
-            class WrapContinuation<T>(val cont: Continuation<T>) : Continuation<T> by cont {
-                override fun resumeWith(result: Result<T>) = wrapBlock { cont.resumeWith(result) }
-            }
-
-            return context + object : AbstractCoroutineContextElement(ContinuationInterceptor), ContinuationInterceptor {
-                override fun <T> interceptContinuation(continuation: Continuation<T>): Continuation<T> {
-                    val wrapped = WrapContinuation(continuation)
-                    return context[ContinuationInterceptor]?.interceptContinuation(wrapped) ?: wrapped
-                }
-            }
-        }
-
-        return GlobalScope.launch(wrapContext(context), block = action)
+        return GlobalScope.launch(context.wrap(::wrapBlock), block = action)
     }
 
 }
