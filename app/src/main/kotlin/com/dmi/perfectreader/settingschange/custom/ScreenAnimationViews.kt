c3d048735ed4c0938b234038f99c0029c2822d76
Igor Demin <igordmn@gmail.com>
2018-10-16 00:12:42 +0300

    Refactoring

--- app/src/main/kotlin/com/dmi/perfectreader/settingschange/custom/ScreenAnimationViews.kt
+++ app/src/main/kotlin/com/dmi/perfectreader/settingschange/custom/ScreenAnimationViews.kt
@@ -1,7 +1,6 @@
 package com.dmi.perfectreader.settingschange.custom
 
 import android.content.Context
-import android.view.View
 import android.widget.ImageView
 import com.dmi.perfectreader.R
 import com.dmi.perfectreader.book.Book
@@ -19,72 +18,53 @@ import com.dmi.util.graphic.Size
 import org.jetbrains.anko.dip
 import org.jetbrains.anko.padding
 import java.net.URI
-import kotlin.reflect.KProperty0
 
 fun screenAnimationDetails(
         context: Context,
         model: SettingsChange,
         book: Book,
         glContext: GLContext
-): View {
-    fun itemView(context: Context) = ScreenAnimationItemView(context, glContext)
-
-    return details(
-            context, model, R.string.settingsChangeScreenAnimation,
-            SettingListView(
-                    context,
-                    context.main.settings.format::pageAnimationPath,
-                    context.main.resources.pageAnimations.map { it.toString() },
-                    ::itemView,
-                    onItemClick = book::showDemoAnimation
-            ).apply {
-                layoutManager = GridAutoFitLayoutManager(context, columnWidth = dip(64 + 12 * 2))
-                setPaddingRelative(dip(12), 0, dip(12), 0)
-            }
-    )
-}
-
-class ScreenAnimationItemView(context: Context, glContext: GLContext) : ImageView(context), Bindable<String> {
-    private val previews = context.main.resources.pageAnimationPreviews(glContext)
-    private val load = ViewLoad(this)
-    private val previewSize = Size(dip(64), dip(64 * 4 / 3F))
-
-    init {
-        minimumWidth = previewSize.width
-        minimumHeight = previewSize.height
-        padding = dip(12)
-    }
-
-    override fun bind(model: String) {
-        val path = URI(model)
-        setImageBitmap(null)
-        load.start {
-            val preview = previews.of(path, previewSize)
-            setImageBitmap(preview)
+) = details(
+        context, model, R.string.settingsChangeScreenAnimation,
+        SettingListView(
+                context,
+                context.main.settings.format::pageAnimationPath,
+                context.main.resources.pageAnimations.map { it.toString() },
+                {
+                    ScreenAnimationPreviewView(context, glContext, size = 64).apply {
+                        padding = dip(12)
+                    }
+                },
+                onItemClick = book::showDemoAnimation
+        ).apply {
+            layoutManager = GridAutoFitLayoutManager(context, columnWidth = dip(64 + 12 * 2))
+            setPaddingRelative(dip(12), 0, dip(12), 0)
         }
-    }
-}
+)
 
-fun screenAnimationPreview(context: Context, glContext: GLContext) = PreviewView(ScreenAnimationPreviewView(context, glContext))
+fun screenAnimationPreview(
+        context: Context, glContext: GLContext
+) = PreviewView(ScreenAnimationPreviewView(context, glContext, size = 24).apply {
+    autorun {
+        bind(context.main.settings.format.pageAnimationPath)
+    }
+})
 
 class ScreenAnimationPreviewView(
         context: Context,
         glContext: GLContext,
-        model: KProperty0<String> = context.main.settings.format::pageAnimationPath
-) : ImageView(context) {
+        size: Int
+) : ImageView(context), Bindable<String> {
     private val previews = context.main.resources.pageAnimationPreviews(glContext)
     private val load = ViewLoad(this)
-    private val previewSize = Size(dip(24), dip(24 * 4 / 3F))
+    private val previewSize = Size(dip(size), dip(size * 4 / 3F))
 
     init {
         minimumWidth = previewSize.width
         minimumHeight = previewSize.height
-        autorun {
-            bind(model.get())
-        }
     }
 
-    private fun bind(model: String) {
+    override fun bind(model: String) {
         val path = URI(model)
         setImageBitmap(null)
         load.start {
