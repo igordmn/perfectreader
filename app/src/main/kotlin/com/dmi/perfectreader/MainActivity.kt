74471e45c8e97ef371d6ba55596e5744b90fd57c
Igor Demin <igordmn@gmail.com>
2018-11-03 19:57:40 +0300

    Launch activity

--- app/src/main/kotlin/com/dmi/perfectreader/MainActivity.kt
+++ app/src/main/kotlin/com/dmi/perfectreader/MainActivity.kt
@@ -1,24 +1,14 @@
 package com.dmi.perfectreader
 
+import android.app.Activity
 import android.content.Intent
-import com.dmi.perfectreader.reader.ReaderLoad
-import com.dmi.perfectreader.reader.ReaderLoadState
-import com.dmi.perfectreader.reader.readerLoadView
-import com.dmi.util.android.view.ActivityExt
-import kotlinx.serialization.cbor.CBOR.Companion.dump
-import kotlinx.serialization.cbor.CBOR.Companion.load
+import android.os.Bundle
+import com.dmi.perfectreader.reader.ReaderActivity
 
-class MainActivity : ActivityExt<ReaderLoad>() {
-    override fun createModel(stateData: ByteArray?) = ReaderLoad(main, intent, ::back, loadState(stateData))
-    override fun saveModel(model: ReaderLoad) = saveState(model.state)
-    override fun view(model: ReaderLoad) = readerLoadView(this, model)
-    private fun loadState(stateData: ByteArray?): ReaderLoadState = if (stateData != null) load(stateData) else ReaderLoadState()
-    private fun saveState(state: ReaderLoadState): ByteArray = dump(state)
-    private fun back() = finish()
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        setIntent(intent)
-        recreateModel()
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, ReaderActivity::class.java))
+        finish()
     }
 }
---
+++ app/src/main/kotlin/com/dmi/perfectreader/reader/ReaderActivity.kt
@@ -0,0 +1,22 @@
+package com.dmi.perfectreader.reader
+
+import android.content.Intent
+import com.dmi.perfectreader.main
+import com.dmi.util.android.view.ActivityExt
+import kotlinx.serialization.cbor.CBOR.Companion.dump
+import kotlinx.serialization.cbor.CBOR.Companion.load
+
+class ReaderActivity : ActivityExt<ReaderLoad>() {
+    override fun createModel(stateData: ByteArray?) = ReaderLoad(main, intent, ::back, loadState(stateData))
+    override fun saveModel(model: ReaderLoad) = saveState(model.state)
+    override fun view(model: ReaderLoad) = readerLoadView(this, model)
+    private fun loadState(stateData: ByteArray?): ReaderLoadState = if (stateData != null) load(stateData) else ReaderLoadState()
+    private fun saveState(state: ReaderLoadState): ByteArray = dump(state)
+    private fun back() = finish()
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        recreateModel()
+    }
+}
